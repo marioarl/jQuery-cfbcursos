d8d1b2886ebaf3276db83fc770829827319507b1
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2022-03-13 11:15:47 -0300

    Criacao script.js

---
+++ aula 11 - Metodo parent/script.js
@@ -0,0 +1,7 @@
+
+//Metodo parent
+$('p').parent().css({'border':'1px solid #f00'}) //Aplicada uma borda vermelha no elemento PAI do <p>, que é o <li>
+$('li').parent().css({'border':'1px solid #00f'})//Aplicada uma borda azul no elemento PAI do <li>, que é o <UL>
+$('ul').parent().css({'border':'1px solid #0f0'})//Aplicada uma borda verde no elemento PAI do <ul>, que é o <div>
+$('div').parent().css({'border':'1px solid #444'})//Aplicada uma borda azul no elemento PAI do <div>, que é o <body>
+$('body').parent().css({'border':'1px solid #f0f'})//Aplicada uma borda azul no elemento PAI do <body>, que é o <html>
