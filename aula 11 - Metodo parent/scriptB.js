8aad976d8dac241fed456b321586b2d63fe32cd2
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2022-03-13 11:16:22 -0300

    Criacao scriptB.js

---
+++ aula 11 - Metodo parent/scriptB.js
@@ -0,0 +1,2 @@
+//Metodo parents until
+$('p').parentsUntil('div').css({'border':'1px solid #f00'}) //Aplicada uma borda vermelha em todos os elementos acima do <p> até o <div>, que são <li> <ul> <div>
