fcc352022566e019e40e05c9f7567edb6503ecef
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2022-03-13 11:35:38 -0300

    Criacao script.js

---
+++ aula 12 - children e find/script.js
@@ -0,0 +1,4 @@
+//children
+
+$('div').children().css({'border':'1px solid #f00'})//Aplicada uma borda vermelha nos elementos FILHO DIRETO da <div>, que é a <ul>
+$('ul').children().css({'border':'1px solid #0f0'})//Aplicada uma borda verde nos elementos FILHO DIRETO da <ul>, que é a <li>
