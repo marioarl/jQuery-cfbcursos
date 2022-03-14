de899022139e3472f539512d00bbd08495d1221a
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2022-03-14 09:40:01 -0300

    Criacao scriptA.js

---
+++ aula 13 - Sibilings next e prev/scriptA.js
@@ -0,0 +1,7 @@
+//Next, é o elemento que está abaixo do elemento que estamos apontando, ou seja, o proximo elemento
+
+$('h3').next().css({'border':'1px solid #00f'})//Aplicada uma borda azul no proximo elemento que está no mesmo nivel do <h3> (exceto <h3>), que é o <p>
+
+//prev, é o elemento que está acima do elemento que estamos apontando, ou seja, o elemento anterior
+
+$('h1').prev().css({'border':'1px solid #f00'})//Aplicada uma borda vermelha no elemento anterior que está no mesmo nivel do <h1> (exceto <h1>), que é o <p>
