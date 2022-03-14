87ff877cfb24e29224e3f76c73feeec42ac21f0e
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2022-03-14 09:40:22 -0300

    Criacao scriptC.js

---
+++ aula 13 - Sibilings next e prev/scriptC.js
@@ -0,0 +1,3 @@
+//nextAll(), todos os elementos que estão abaixo do elemento que estamos apontando, ou seja, os elementos posteriores
+
+$('h3').nextAll().css({'border':'1px solid #f00'})//Aplicada uma borda vermelha nos elementos posteriores que estão no mesmo nivel do <h3> (exceto <h3>), que são o <p> <h2> <p> <h1>
