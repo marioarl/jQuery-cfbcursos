939ddf8760291df44c9b2ff632f7fda394eb30f6
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2022-03-14 09:40:12 -0300

    Criacao scriptB.js

---
+++ aula 13 - Sibilings next e prev/scriptB.js
@@ -0,0 +1,3 @@
+//prevAll(), todos os elementos que estão acima do elemento que estamos apontando, ou seja, os elementos anteriores
+
+$('h1').prevAll().css({'border':'1px solid #f00'})//Aplicada uma borda vermelha nos elementos anteriores que estão no mesmo nivel do <h1> (exceto <h1>), que é o <p> <h2> <p> <h3>
