1f547d2992f7adc4037459fe41f4a8ece625ce26
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2022-03-22 11:52:41 -0300

    Criacao script.js

---
+++ aula 15 - Filtragem de elementos - filter, not/script.js
@@ -0,0 +1,3 @@
+$('p').filter('.borda').css({'color':'#f00'}) //filter()Elementos que tenham uma classe, atributo em especifico
+
+$('p').not('.borda').css({'color':'#00f'}) //Contrario do FILTER, todos os <p> que não tenham a classe borda, ficarao com o texto azul
