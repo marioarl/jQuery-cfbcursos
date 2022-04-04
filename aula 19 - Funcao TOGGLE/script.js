e3aee51d433342aecda2ad8eceeb251ac849d869
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2022-04-04 10:19:51 -0300

    Criacao script.js

---
+++ aula 19 - Funcao TOGGLE/script.js
@@ -0,0 +1,10 @@
+$("#btn_mostrarocultar").click(
+    function(){
+       $('#caixa').toggle()
+       if($('#caixa').is(':visible')){
+        $("#btn_mostrarocultar").text('Ocultar')
+       }else{
+        $("#btn_mostrarocultar").text('Mostrar')
+       }
+    }
+)
