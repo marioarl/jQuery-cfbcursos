70d65bc91ba4aee495ecd1ae7e4bcdf94f2f60f4
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2022-04-04 10:04:40 -0300

    Criacao script.js

---
+++ aula 17 - Obter e modificar valores de atributos/script.js
@@ -0,0 +1,5 @@
+$('#caixa').click(
+    function(){
+        alert("Atributo: " + $(this).attr('style')) //dentro da funcao attr, podemos colocar varios paramentros como : o id, style e etc
+    }
+)
